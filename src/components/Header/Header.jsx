a313d55842f2dc37b1bc72e220fbb605cc71dd40
Chris Mo <cmochinski@gmail.com>
2021-09-20 11:58:23 -0500

    quick cursor change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
         <div className="container">
             <header className="App-header">
                 <h1 className="App-title">My European Tour Gallery</h1>
-                <h4 className="App-subtitle">In late 2019, <a href="http://www.junkfm.com" target="_blank">Junk FM</a> departed for a two-week European New Years Eve tour, playing mostly for our awesome armed forces across the content.</h4>
+                <h4 className="App-subtitle">In late 2019, <a href="http://www.junkfm.com" target="_blank">Junk FM</a> departed for a two-week New Years Eve tour, performing for our awesome armed forces across Europe.</h4>
             </header>
         </div>
     )
